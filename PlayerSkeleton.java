105c088b7cac8853dc9beb607fd674da12462273
olenhad <momeriqbal2@gmail.com>
2013-03-22 06:51:05 +0800

    init

---
+++ PlayerSkeleton.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+public class PlayerSkeleton {
+
+    double[] curFeatures = new double[Features.NUMBER_FEATURES];
+    double[] prevFeautures;
+    private Features featureFn = new Features();
+    private NextState ns = null;
+
+	//implement this function to have a working system
+	public int pickMove(State s, int[][] legalMoves) {
+		if (ns == null) ns = new NextState();
+        ns.resetState(s);
+        int maxMove = maxUtilityMove(s,legalMoves,curFeatures);
+
+        return maxMove;
+                
+	}
+	public int maxUtilityMove(State s, int[][] legalMoves, double[] features) {
+        double maxScore = Double.NEGATIVE_INFINITY;
+        int start = (int)(Math.random()*legalMoves.length);
+        int move = (start+1)%(legalMoves.length);
+        int maxMove = move;
+        double score;
+        while (move != start) {
+            ns.makeMove(move);
+            if (! ns.hasLost()) {
+                score = featureFn.score(s,ns, legalMoves[move]);
+                if (maxScore < score) {
+                    maxMove = move;
+                    maxScore = score;
+                }
+            }
+            ns.resetState(s);
+            move = (move+1)%(legalMoves.length);
+        }
+        return maxMove;
+    }
+	public static void main(String[] args) {
+		State s = new State();
+		new TFrame(s);
+		PlayerSkeleton p = new PlayerSkeleton();
+		while(!s.hasLost()) {
+			s.makeMove(p.pickMove(s,s.legalMoves()));
+			s.draw();
+			s.drawNext(0,0);
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+	}
+	
+}
