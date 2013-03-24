0f6c10c2e850c2278bf189d810efb3bf2d787512
Arnold Koroa <arnold.koroa@gmail.com>
2013-03-24 12:24:55 +0800

    Init Arnold's "manual" version

--- State.java
+++ State.java
@@ -22,7 +22,7 @@ public class State {
 	private int turn = 0;
 	private int cleared = 0;
 	
-	//each square in the grid - int means empty - other values mean the turn it was placed
+	//each square in the grid - 0 means empty - other values mean the turn it was placed
 	private int[][] field = new int[ROWS][COLS];
 	//top row+1 of each column
 	//0 means empty
