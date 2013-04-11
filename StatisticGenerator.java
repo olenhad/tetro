8ac5dbb2bc6ece73136d6846f08f6fbd0a8b84a3
Arnold Koroa <arnold.koroa@gmail.com>
2013-04-12 02:55:19 +0800

    Fixed silly bugs whichis caused by forgetting that in Java everything is a reference. I've been coding in C++ too much..

--- StatisticGenerator.java
+++ StatisticGenerator.java
@@ -62,49 +62,60 @@ public class StatisticGenerator {
                 -30,		/* SUM_ABS_HEIGHT_DIFF */
                 -20			/* DELTA_SUM_ABS_HEIGHT_DIFF */ 
         		};
-		double[] gen1 = {486.675, 100.0, -3.36, -4.82, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
-		double[] gen2 = {105.43253815742321, 30.83704520472885, -0.9297627843871829, -1.1841151106423722, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
-		double[] gen3 = {32.48581083059224, 27.23001322384344, -0.25561010883341245, -1.1841151106423722, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
-		double[] gen4 = {0.9266257041073647, 0.837484482310802, -0.005298893029129517, -0.043717150606703115, -1.5733348100669897, -5.404692237085569, -0.13511509864342033, -2.414533892953383, -1.0021306562633423, -0.6055850494278614};
-		double[] w1 = gen3, w2 = gen4;
+		
+		double[][] gen = new double[10][];
+		gen[1] = new double[] {486.675, 100.0, -3.36, -4.82, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
+		gen[2] = new double[] {105.43253815742321, 30.83704520472885, -0.9297627843871829, -1.1841151106423722, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
+		gen[3] = new double[] {32.48581083059224, 27.23001322384344, -0.25561010883341245, -1.1841151106423722, -68.4, -200.0, -5.0, -100.0, -30.0, -20.0};
+		gen[4] = new double[] {0.9266257041073647, 0.837484482310802, -0.005298893029129517, -0.043717150606703115, -1.5733348100669897, -5.404692237085569, -0.13511509864342033, -2.414533892953383, -1.0021306562633423, -0.6055850494278614};
+		gen[5] = new double[] {0.021596608846013502, 0.014249672906482562, -1.3661955331280978E-4, -9.534315780956981E-4, -0.03915677305256805, -0.09241272364259767, -0.0031985161670923736, -0.05915856517255117, 0.03467310044516623, -0.02260961512491643};
+		gen[6] = new double[] {3.158594275364314, 2.6475732539183303, -0.024852962134660882, 0.08716397231688221, -6.650529659289416, -19.44599315581701, -0.4861498288954254, -9.722996577908505, -2.9168989733725503, -1.9445993155817016};
+		gen[0] = new double[] {2.5019225255160737, 2.0971427744287094, -0.019686031306864884, 0.0690425824722024, -5.267884543123147, -15.403171178722655, -0.38507927946806647, -7.701585589361327, -2.310475676808397, -1.5403171178722659};
+		
+		double[] w1 = gen[6], w2 = gen[0];
 		double[] goodPlayer = null, badPlayer = null, goodScore = null, badScore = null;
 		
 		double bestScore = 0;
 		double[] bestPlayer = null;
 		for(int i=0;i < BREED_ITERATION; i++){
 			System.out.println("Starting generation " + i + "...");
+			System.out.println("\nBest player so far of avg score: " + bestScore);
+			System.out.println("stats: " + Arrays.toString(bestPlayer));
+			System.out.println();
 			double[] score1 = getStats(w1);
 			double[] score2 = getStats(w2);
 			
 			if(score1[0] > bestScore){
 				bestScore = score1[0];
-				bestPlayer = w1;
+				bestPlayer = Arrays.copyOf(w1, 10);
 			}
 			if(score2[0] > bestScore){
 				bestScore = score2[0];
-				bestPlayer = w2;
+				bestPlayer = Arrays.copyOf(w2, 10);;
 			}
 			
 			boolean changed = false;
-			if(score1[0] < 150){
-				w1 = bestPlayer;
+			if(score1[0] < bestScore/5){
+				w1 = Arrays.copyOf(bestPlayer, 10);
+				w2 = Arrays.copyOf(gen[randGen.nextInt(7)], 10);
 				changed = true;
 			}
-			if(score2[0] < 150){
-				w2 = bestPlayer;
+			if(score2[0] < bestScore/5){
+				w1 = Arrays.copyOf(gen[randGen.nextInt(7)], 10);
+				w2 = Arrays.copyOf(bestPlayer, 10);
 				changed = true;
 			}
 			if(changed) continue;
 			
 			if(better(score1,score2)) {
-				goodPlayer = w1; 
+				goodPlayer = Arrays.copyOf(w1, 10); 
 				goodScore = score1;
-				badPlayer = w2;
+				badPlayer = Arrays.copyOf(w2, 10);
 				badScore = score2;
 			} else {
-				goodPlayer = w2; 
+				goodPlayer = Arrays.copyOf(w2, 10);
 				goodScore = score2;
-				badPlayer = w1;
+				badPlayer = Arrays.copyOf(w1, 10);
 				badScore = score1;
 			}
 
@@ -123,8 +134,6 @@ public class StatisticGenerator {
 			}
 			
 			printStats(goodPlayer, badPlayer, goodScore, badScore);
-			System.out.println("Best player of avg score: " + bestScore);
-			System.out.println("stats: " + Arrays.toString(bestPlayer));
 		}
 		
 		System.out.println("Results after " + BREED_ITERATION + " generations:");
